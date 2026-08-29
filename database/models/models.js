c289a895ebfac906174c88587642d4553a0ffdf1
Carlos Oliveira <carlos.oliveira.dev.junior@gmail.com>
2026-08-29 15:27:48 -0300

    Product e Supplier agora tem a chave userId para identificar quem cadastrou cada tipo

--- database/models/models.js
+++ database/models/models.js
@@ -12,6 +12,24 @@ Profile.belongsTo(User, {
   foreignKey: 'userId'
 });
 
+User.hasMany(Product, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+Product.belongsTo(User, {
+  foreignKey: 'userId'
+});
+
+User.hasMany(Supplier, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+Supplier.belongsTo(User, {
+  foreignKey: 'userId'
+});
+
 Product.belongsToMany(Supplier, { 
   through: 'ProductSupplier',
   foreignKey: 'productId'
